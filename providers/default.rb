6e70d8b7276c2dba27fdbe087d6b50c6ffa64f94
Arturo Guzman <arturo@kisurfer.com>
2012-07-30 00:27:31 +0000

    Rename reference resource to revision

--- providers/default.rb
+++ providers/default.rb
@@ -3,22 +3,26 @@ def windows?
 end
 
 action :sync do
-  raise "Sync is not supported in windows" if windows?
-  execute "sync repository #{new_resource.path}" do
-    not_if "hg identify #{new_resource.path}"
-    command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
-  end
-  execute "pull changes #{new_resource.path}" do
-      command "cd #{new_resource.path} && hg pull -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository}"
-  end
-  execute "update #{new_resource.path}" do
-      command "cd #{new_resource.path} && hg update -r #{new_resource.reference}"
-  end
-  execute "sync update owner #{new_resource.path}" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
-  end
-  execute "sync update permissions #{new_resource.path}" do
-    command "chmod -R #{new_resource.mode} #{new_resource.path}"
+  case node[:platform]
+  when "windows"
+    raise "Sync is not supported in windows"
+  else
+    execute "sync repository #{new_resource.path}" do
+      not_if "hg identify #{new_resource.path}"
+      command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
+    end
+    execute "pull changes #{new_resource.path}" do
+        command "cd #{new_resource.path} && hg pull -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository}"
+    end
+    execute "update #{new_resource.path}" do
+        command "cd #{new_resource.path} && hg update -r #{new_resource.revision}"
+    end
+    execute "sync update owner #{new_resource.path}" do
+      command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
+    end
+    execute "sync update permissions #{new_resource.path}" do
+      command "chmod -R #{new_resource.mode} #{new_resource.path}"
+    end
   end
 end
 
@@ -28,8 +32,8 @@ action :clone do
     not_if "hg identify #{new_resource.path}"
     command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
   end
-  if new_resource.reference
-      command "cd #{new_resource.path} && hg update -r #{new_resource.reference}"
+  if new_resource.revision
+      command "cd #{new_resource.path} && hg update -r #{new_resource.revision}"
   end
   execute "update owner #{new_resource.path}" do
     command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
--- resources/default.rb
+++ resources/default.rb
@@ -2,7 +2,7 @@ actions :sync, :clone
 
 attribute :path, :kind_of => String, :name_attribute => true
 attribute :repository, :kind_of => String
-attribute :reference, :kind_of => [Integer, String]
+attribute :revision, :kind_of => [Integer, String]
 attribute :key, :kind_of => String
 attribute :owner, :kind_of => String
 attribute :group, :kind_of => String
