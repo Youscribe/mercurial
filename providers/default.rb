255cb3d49e2a7506064994ba7fd6e03005f0a2e4
Arturo Guzman <arturo@kisurfer.com>
2012-07-30 17:53:54 +0000

    Add basic windows support for clone and sync actions

--- providers/default.rb
+++ providers/default.rb
@@ -3,42 +3,62 @@ def windows?
 end
 
 action :sync do
-  case node[:platform]
-  when "windows"
-    raise "Sync is not supported in windows"
-  else
-    execute "sync repository #{new_resource.path}" do
-      not_if "hg identify #{new_resource.path}"
+  execute "#{new_resource} sync repository #{new_resource.path}" do
+    not_if "hg identify #{new_resource.path}"
+    if windows?
+      command "hg clone #{new_resource.repository} #{new_resource.path}"
+    else
       command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
     end
-    execute "pull changes #{new_resource.path}" do
-        command "cd #{new_resource.path} && hg pull -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository}"
+  end
+
+  execute "#{new_resource} pull changes #{new_resource.path}" do
+    command "cd #{new_resource.path}"
+    if windows?
+      command "hg pull #{new_resource.repository}"
+    else
+      command "hg pull -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository}"
     end
-    execute "update #{new_resource.path}" do
-        command "cd #{new_resource.path} && hg update -r #{new_resource.revision}"
-    end
-    execute "sync update owner #{new_resource.path}" do
+  end
+
+  execute "#{new_resource} update #{new_resource.path}[#{new_resource.revision.to_s}]" do
+    command "cd #{new_resource.path}"
+    command "hg update -r #{new_resource.revision}"
+  end
+
+  # TODO: Add permissions support in windows
+  unless windows?
+    execute "#{new_resource} sync update owner #{new_resource.path}" do
       command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
     end
-    execute "sync update permissions #{new_resource.path}" do
+    execute "#{new_resource} sync update permissions #{new_resource.path}" do
       command "chmod -R #{new_resource.mode} #{new_resource.path}"
     end
   end
 end
 
 action :clone do
-  raise "Clone is not supported in windows" if windows?
-  execute "clone repository #{new_resource.path}" do
+  execute "#{new_resource} clone repository #{new_resource.path}" do
     not_if "hg identify #{new_resource.path}"
-    command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
+    if windows?
+      command "hg clone #{new_resource.repository} #{new_resource.path}"
+    else
+      command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
+    end
   end
+
   if new_resource.revision
-      command "cd #{new_resource.path} && hg update -r #{new_resource.revision}"
+      command "cd #{new_resource.path}"
+      command "hg update -r #{new_resource.revision}"
   end
-  execute "update owner #{new_resource.path}" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
-  end
-  execute "update permissions #{new_resource.path}" do
-    command "chmod -R #{new_resource.mode} #{new_resource.path}"
+
+  # TODO: Add permissions support in windows
+  unless windows?
+    execute "#{new_resource} update owner #{new_resource.path}" do
+      command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
+    end
+    execute "#{new_resource} update permissions #{new_resource.path}" do
+      command "chmod -R #{new_resource.mode} #{new_resource.path}"
+    end
   end
 end
