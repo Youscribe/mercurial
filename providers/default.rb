ddddd9bfe242ce639370abeec430199d3657cc91
Arturo Guzman <arturo@kisurfer.com>
2012-07-31 08:34:55 +0000

    Fix actions for windows

--- providers/default.rb
+++ providers/default.rb
@@ -13,17 +13,17 @@ action :sync do
   end
 
   execute "#{new_resource} pull changes #{new_resource.path}" do
-    command "cd #{new_resource.path}"
     if windows?
-      command "hg pull #{new_resource.repository}"
+      command "cd #{new_resource.path} && hg pull #{new_resource.repository}"
     else
-      command "hg pull -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository}"
+      command "cd #{new_resource.path} && hg pull -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository}"
     end
   end
 
   execute "#{new_resource} update #{new_resource.path}[#{new_resource.revision.to_s}]" do
-    command "cd #{new_resource.path}"
-    command "hg update -r #{new_resource.revision}"
+    revision = new_resource.revision
+    update_command_args = "-r #{revision}" unless revision.nil?
+    command "cd #{new_resource.path} && hg update #{update_command_args}"
   end
 
   # TODO: Add permissions support in windows
@@ -39,17 +39,15 @@ end
 
 action :clone do
   execute "#{new_resource} clone repository #{new_resource.path}" do
-    not_if "hg identify #{new_resource.path}"
     if windows?
-      command "hg clone #{new_resource.repository} #{new_resource.path}"
+      command "cd #{new_resource.path} && hg clone #{new_resource.repository} #{new_resource.path}"
     else
-      command "hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
+      command "cd #{new_resource.path} && hg clone -e 'ssh -i #{new_resource.key} -o StrictHostKeyChecking=no' #{new_resource.repository} #{new_resource.path}"
     end
   end
 
   if new_resource.revision
-      command "cd #{new_resource.path}"
-      command "hg update -r #{new_resource.revision}"
+      command "cd #{new_resource.path} && hg update -r #{new_resource.revision}"
   end
 
   # TODO: Add permissions support in windows
